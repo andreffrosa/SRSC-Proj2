3821fedcbf4991a9eeab3fb462cc545ed2b2d01f
André Rosa <af.rosa@campus.fct.unl.pt>
2019-05-13 14:38:58 +0100

    Start Client console

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,36 @@
 package client;
 
+import java.util.Scanner;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// Receive from args the location of the configuration files
+		
+		// Process user commands
+		Scanner in = new Scanner(System.in);
+		
+		String cmd;
+		
+		boolean exit = false;
+		while(!exit) {
+			System.out.print("> ");
+			cmd = in.next();
+			
+			switch(cmd) {
+			case "login":
+				String username = in.next();
+				String password = in.next();
+				break;
+			case "exit":
+				exit = true;
+				break;
+			}
+			
+			in.nextLine();
+		}
+		System.out.println("Exiting...");
 	}
-
+	
 }
