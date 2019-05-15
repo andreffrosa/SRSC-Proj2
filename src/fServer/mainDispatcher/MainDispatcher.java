904bc2e2e723dae2adb230ff4e0d9917a7a10d7b
André Rosa <af.rosa@campus.fct.unl.pt>
2019-05-15 10:33:07 +0100

    Implemented HTTPS utility library

--- src/fServer/mainDispatcher/MainDispatcher.java
+++ src/fServer/mainDispatcher/MainDispatcher.java
@@ -1,96 +1,133 @@
+/**
+ * 
+ */
 package fServer.mainDispatcher;
 
-import java.net.URI;
-import java.util.Properties;
+import java.util.List;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;	
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
+/**
+ * @author Ruben Silva & Andre Rosa
+ *
+ */
 
-import com.sun.net.httpserver.HttpsServer;
+//TODO: Register, Exceptions a enviar
 
-import utility.IO;
-import utility.tls.ClientCertificateVerifier;
+@Path(MainDispatcher.PATH)
+public interface MainDispatcher {
 
-@SuppressWarnings("restriction")
-public class MainDispatcher {
-
-	public static void main(String[] args) throws Exception {
-
-		if (args.length < 3) {
-			System.err.println("Usage: MainDispatcher <port> <tls-configs> <keystore-configs> <service-endpoints>");
-			System.exit(-1);
-		}
-
-		int port = Integer.parseInt(args[0]);
-
-		// Read Configs
-		Properties tls_properties = IO.loadProperties(args[1]);
-		
-		// TODO: ler as configs tls 
-		
-		// Read Keystore Properties
-		Properties keystore_properties = IO.loadProperties(args[2]);
-		
-		// TODO: Transformar em constantes
-		String server_keystore = keystore_properties.getProperty("keystore");
-		String keystore_password = keystore_properties.getProperty("keystore-password");
-		String keystore_type = keystore_properties.getProperty("keystore-type");
-		String server_truststore = keystore_properties.getProperty("truststore");
-		String truststore_password = keystore_properties.getProperty("truststore-password");
-		String truststore_type = keystore_properties.getProperty("truststore-type");
-		
-		System.setProperty("java.net.preferIPv4Stack", "true");
-		System.setProperty("javax.net.ssl.keyStore", server_keystore);
-		System.setProperty("javax.net.ssl.keyStorePassword", keystore_password);
-		System.setProperty("javax.net.ssl.trustStore", server_truststore);
-		System.setProperty("javax.net.ssl.trustStorePassword", truststore_password);
-		
-		// Read Endpoints
-		Properties service_endpoints = IO.loadProperties(args[3]);
-		
-		// TODO: read the loation of the other services
-		
-		
-		//
-		
-		boolean authenticate_clients = false;
-
-		//DistributedWallet wallet = new BFTReplicatedWallet(id, byzantine);
-
-		URI baseUri = UriBuilder.fromUri("https://0.0.0.0/").port(port).build();
-
-		HttpsServer server = null;
-		SSLContext ctx = null;
-
-		ResourceConfig config = new ResourceConfig();
-		config.register(wallet);
-
-		if (authenticate_clients) {
-			ClientCertificateVerifier ccv = new ClientCertificateVerifier();
-			ctx = ccv.init(server_keystore, keystore_password, keystore_password, server_truststore, truststore_password, truststore_type, "TLSv1.2");
-
-			HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
-
-			server = (HttpsServer) JdkHttpServerFactory.createHttpServer(baseUri, config, ctx, false);
-
-			ccv.configureHttps(server);
-		} else {
-			ctx = SSLContext.getDefault();
-
-			server = (HttpsServer) JdkHttpServerFactory.createHttpServer(baseUri, config, ctx, false);
-		}
-
-		server.start();
-
-		System.out.println("\n\t#######################################################"
-				         + "\n\t      MainDispatcher ready @ " + baseUri 
-				         + "\n\t      Client Authentication: " + authenticate_clients 
-			             + "\n\t#######################################################");
-
-	}
-
+	String PATH = "/dispacher";
+	String CHARSET = ";charset=utf-8";
+	
+	/**
+	 * Endpoint that allows users to authenticate themselves.
+	 * @param username user username
+	 * @param password user password
+	 * @return True if everything if correct (and a token on the authorization header), false if credentials are wrong.
+	 */
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean login(String username, String password);
+	
+	/**
+	 *  List files or directories on the specified path
+	 * @param username user username 
+	 * @param path path to list files
+	 * @return A list of names of the files and subdirectories on that path.
+	 */
+	@GET
+	@Path("/ls/{username}/{path}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public List<String> listFiles(@PathParam("username") String username,@PathParam("path") String path);
+	
+	/**
+	 * Creates a directory on the specified path.
+	 * @param username user username
+	 * @param path path to create directory
+	 * @return True if everything went correctly or false if the directory was not created.
+	 */
+	@POST
+	@Path("/mkdir/{username}/{path}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean mkdir(@PathParam("username") String username, @PathParam("path") String path);
+	
+	/**
+	 * Uploads a file to the specified directory.
+	 * @param username user username.
+	 * @param path path where to upload file.
+	 * @param data file data as byte array.
+	 * @return True if everything went correctly or false if the file was not uploaded.
+	 */
+	@PUT
+	@Path("/put/{username}/{path}")
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean upload(@PathParam("username") String username, @PathParam("path") String path, byte[] data );
+	
+	/**
+	 * Downloads a file that resides in the specified directory
+	 * @param username user username
+	 * @param path file path
+	 * @return A byte array with the file data.
+	 */
+	@GET
+	@Path("/get/{username}/{path}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public byte[] download(@PathParam("username") String username, @PathParam("path") String path);
+	
+	/**
+	 * Copies a file from some path1 to some other path2.
+	 * @param username user username.
+	 * @param origin path of the file.
+	 * @param dest path where to copy to.
+	 * @return True if everything went correctly or false if the file was not copied.
+	 */
+	@PUT
+	@Path("/cp/{username}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean copy(@PathParam("username") String username, @QueryParam("origin") String origin, @QueryParam("dest") String dest);
+	
+	/**
+	 * Removes a file that resides on the specified path.
+	 * @param username user username
+	 * @param path path of the file.
+	 * @return True if everything went correctly or false if the file was not removed.
+	 */
+	@DELETE
+	@Path("/rm/{username}/{path}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean remove(@PathParam("username") String username, @PathParam("path") String path);
+	
+	/**
+	 * Removes a directory with the path provided.
+	 * @param username user username
+	 * @param path path of the directory
+	 * @return True if everything went correctly or false if the directory was not removed.
+	 */
+	@DELETE
+	@Path("/rmdir/{username}/{path}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean removeDirectory(@PathParam("username") String username, @PathParam("path") String path);
+	
+	/**
+	 * Retrieved a file metadata
+	 * @param username user username
+	 * @param path path of the file
+	 * @return An object with the info: isDirectory,type, name, creationDate, lastModification
+	 */
+	@GET
+	@Path("/file/{username}/{path}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET)
+	public boolean getFileMetadata(@PathParam("username") String username, @PathParam("path") String path);
 }
---
+++ src/fServer/mainDispatcher/MainDispatcherServer.java
@@ -0,0 +1,78 @@
+package fServer.mainDispatcher;
+
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.util.Properties;
+
+import com.sun.net.httpserver.HttpsServer;
+
+import utility.HTTPS;
+import utility.IO;
+import utility.MyKeyStore;
+
+@SuppressWarnings("restriction")
+public class MainDispatcherServer {
+
+	public static void main(String[] args) throws Exception {
+
+		if (args.length < 3) {
+			System.err.println("Usage: MainDispatcher <port> <tls-configs> <keystore-configs> <service-endpoints>");
+			System.exit(-1);
+		}
+
+		int port = Integer.parseInt(args[0]);
+
+		// Read Configs
+		Properties tls_properties = IO.loadProperties(args[1]);
+		
+		String tls_version = tls_properties.getProperty("TLS-PROT-ENF");
+		boolean authenticate_clients = tls_properties.getProperty("TLS-AUTH").equals("MUTUAL");
+		String ciphersuites = tls_properties.getProperty("CIPHERSUITES");
+		
+		SecureRandom sr = null; // TODO: obter do ficheiro
+		
+		// Read Keystore Properties
+		Properties keystore_properties = IO.loadProperties(args[2]);
+		
+		// TODO: Transformar em constantes
+		String keystore_path = keystore_properties.getProperty("keystore");
+		String keystore_password = keystore_properties.getProperty("keystore-password");
+		String keystore_type = keystore_properties.getProperty("keystore-type");
+		String truststore_path = keystore_properties.getProperty("truststore");
+		String truststore_password = keystore_properties.getProperty("truststore-password");
+		String truststore_type = keystore_properties.getProperty("truststore-type");
+		
+		System.setProperty("java.net.preferIPv4Stack", "true"); // Aqui ou nas runconfigs?
+	
+		// Acho que já não é preciso isto
+		/*System.setProperty("javax.net.ssl.keyStore", server_keystore);
+		System.setProperty("javax.net.ssl.keyStorePassword", keystore_password);
+		System.setProperty("javax.net.ssl.trustStore", server_truststore);
+		System.setProperty("javax.net.ssl.trustStorePassword", truststore_password);*/
+		
+		KeyStore ks = MyKeyStore.loadKeyStore(keystore_path, keystore_password, keystore_type);
+		KeyStore ts = MyKeyStore.loadKeyStore(truststore_path, truststore_password, truststore_type);
+		
+		// Read Endpoints
+		Properties service_endpoints = IO.loadProperties(args[3]);
+		
+		// TODO: read the loation of the other services
+		
+		
+		//
+
+		MainDispatcher dispatcher = new MainDispatcherImplementation();
+
+		// Passar o endereço 0.0.0.0 também?
+		HttpsServer server = HTTPS.buildServer(dispatcher, ks, keystore_password, ts, port, tls_version, authenticate_clients, ciphersuites, sr);
+		
+		server.start();
+
+		System.out.println("\n\t#######################################################"
+				         + "\n\t      MainDispatcher ready @ " + server.getAddress() 
+				         + "\n\t      Client Authentication: " + authenticate_clients 
+			             + "\n\t#######################################################");
+
+	}
+
+}
