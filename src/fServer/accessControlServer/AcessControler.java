22aaf00f81a1763caa0d1793d8a11f8e10601e72
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-05-27 21:41:39 +0100

    State saving

--- src/fServer/accessControlServer/AcessControler.java
+++ src/fServer/accessControlServer/AcessControler.java
@@ -1,12 +1,19 @@
 package fServer.accessControlServer;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
 public interface AcessControler {
 
 	/**
 	 * Used to assure if a user can read files or not.
 	 * @return True if the user can read files, False otherwise.
 	 */
-	public boolean canRead(String username);
+	@GET
+	@Path("/read/{username}")
+	
+	public boolean canRead(@PathParam("username") String username);
 	
 	/**
 	 * Used to assure if a user can write files or not.
