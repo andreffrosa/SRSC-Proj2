03584102ad149ccece1f229ab37e984929ef4c6d
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-05-27 23:34:02 +0100

    state saving

--- runAccess.sh
+++ runAccess.sh
@@ -1,7 +1,7 @@
 #JAVA="/usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java"
 JAVA=$JAVA_HOME/jre/bin/java
 
-CP="-cp target/classes/:target/dependency/*:../mySecureREST/target/classes/:../mySecureREST/target/classes/*"
+CP="-cp target/classes/:target/dependency/*:../mySecureREST/target/mySecureREST-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
 
 PORT="6666"
 TLS="./configs/fServer/servertls.conf"
