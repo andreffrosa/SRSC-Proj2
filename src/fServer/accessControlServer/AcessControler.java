8855fc41250431baa602cde26ced7e2a22288310
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-06-01 19:33:21 +0100

    Access with token verifier.

--- src/fServer/accessControlServer/AcessControler.java
+++ src/fServer/accessControlServer/AcessControler.java
@@ -1,6 +1,11 @@
 package fServer.accessControlServer;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.SignatureException;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,11 +26,14 @@ public interface AcessControler {
 	/**
 	 * Used to assure if a user can read files or not.
 	 * @return True if the user can read files, False otherwise.
+	 * @throws SignatureException 
+	 * @throws InvalidKeyException 
+	 * @throws IOException 
 	 */
 	@GET
 	@Path("/{operation}/{username}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public boolean hasAccess(@PathParam("operation") String opeartion, @PathParam("username") String username);
+	public boolean hasAccess(@HeaderParam("Authorization") String token, @PathParam("operation") String opeartion, @PathParam("username") String username) throws InvalidKeyException, SignatureException, IOException;
 	
 	
 		
--- src/fServer/accessControlServer/AcessControllerImplementation.java
+++ src/fServer/accessControlServer/AcessControllerImplementation.java
@@ -3,12 +3,15 @@ package fServer.accessControlServer;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.SignatureException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 
+import fServer.authServer.AuthenticationToken;
 import fServer.authServer.TokenVerifier;
 
 public class AcessControllerImplementation implements AcessControler {
@@ -57,22 +60,40 @@ public class AcessControllerImplementation implements AcessControler {
 		
 	private boolean canRead(String username) {
 		
-		if(permissionsMap.containsKey(username))
-			return permissionsMap.get(username)[READ_POS];
+		System.out.print("Recieved read access verification for user " + username + ". Result: ");
 		
+		if(permissionsMap.containsKey(username)) 
+			if(permissionsMap.get(username)[READ_POS]) {
+				System.out.println("granted");
+				return true;
+			}
+			
+		System.out.println("denied");
 		return false;
 	}
 
 	private boolean canWrite(String username) {
 		
-		if(permissionsMap.containsKey(username))
-			return permissionsMap.get(username)[WRITE_POS];
+		System.out.print("Recieved write access verification for user " + username + ". Result: ");
 		
+		if(permissionsMap.containsKey(username))
+			if(permissionsMap.get(username)[WRITE_POS]) {
+				System.out.println("granted");				
+				return true;
+			}
+		
+		System.out.println("denied");		
 		return false;
 	}
 
 	@Override
-	public boolean hasAccess(String opeartion, String username) {
+	public boolean hasAccess(String token, String opeartion, String username) throws InvalidKeyException, SignatureException, IOException {
+		
+		AuthenticationToken auth = AuthenticationToken.parseToken(token);
+		if(!tokenVerifier.validateToken(System.currentTimeMillis(), auth)) {
+			System.out.println("User " + username + " presented invalid token");
+			return false;
+		}
 		
 		if(opeartion.equals(AcessControler.WRITE_ACCESS_REQUEST))
 			return canWrite(username);
