b1016d16e3a5ce50e6081cda4e92ae6508064f37
André Rosa <af.rosa@campus.fct.unl.pt>
2019-06-19 23:15:52 +0100

    Created config files

--- src/client/proxy/EncryptedFileSystem.java
+++ src/client/proxy/EncryptedFileSystem.java
@@ -7,18 +7,24 @@ import java.nio.file.attribute.FileTime;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
+import java.security.KeyStore;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.KeyStore.PrivateKeyEntry;
+import java.security.KeyStoreException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Map.Entry;
 
 import javax.crypto.BadPaddingException;
@@ -36,6 +42,8 @@ import client.proxy.inodes.FileDescriptor;
 import client.proxy.inodes.FileDescriptor.FragmentMetaData;
 import client.proxy.inodes.Inode;
 import utility.Cryptography;
+import utility.IO;
+import utility.MyKeyStore;
 
 public class EncryptedFileSystem {
 
@@ -45,12 +53,12 @@ public class EncryptedFileSystem {
 	private int iv_size;
 	private String cipher_algorithm;
 	private String cipher_provider;
-	
+
 	private Signature sig;
 	private KeyPair myKeyPair;
 	private MessageDigest hash_function;
 	private SecureRandom sr;
-	
+
 	private DirectoryInode root;
 	private int fragment_size;
 	private int encrypted_payload_size;
@@ -61,7 +69,7 @@ public class EncryptedFileSystem {
 		this.root = new Directory(""); // Root does not have a name
 		this.hash_function = hash_function;
 		this.fragment_size = fragment_size;
-		
+
 		this.secret_key_gen_algorithm = secret_key_gen_algorithm;
 		this.secret_key_gen_provider = secret_key_gen_provider;
 		this.secret_key_size = secret_key_size;
@@ -72,13 +80,13 @@ public class EncryptedFileSystem {
 		this.myKeyPair = myKeyPair;
 		this.hash_function = hash_function;
 		this.sr = sr;
-		
+
 		// TODO: Discover a smartest way to do this
 		SecretKey ks = Cryptography.generateSecretKey(this.secret_key_gen_algorithm, this.secret_key_gen_provider, this.secret_key_size);
 		byte[] iv = Cryptography.createIV(sr, this.iv_size);
 		this.encrypted_payload_size = getCipher(ks, iv, Cipher.ENCRYPT_MODE).getOutputSize(fragment_size);
 	}
-	
+
 	private Cipher getCipher(SecretKey ks, byte[] iv, int mode) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchProviderException {
 		if(this.cipher_provider != null)
 			return Cryptography.buildCipher(this.cipher_algorithm, mode, ks, iv, this.cipher_provider);
@@ -91,7 +99,7 @@ public class EncryptedFileSystem {
 		String s = java.util.Base64.getEncoder().encodeToString(hash);
 		return s.replaceAll("/", "-");
 	}
-	
+
 	public List<String> listFiles(String path) throws FileNotFoundException {
 
 		Inode i = this.root.getInode(path);
@@ -128,25 +136,25 @@ public class EncryptedFileSystem {
 		if(i!= null) {
 			if(i.isDirectory()) {
 				int n_fragments = DataFragment.getNfragments(data.length, fragment_size);
-				
+
 				FragmentMetaData[] fragments_meta = new FragmentMetaData[n_fragments];
 				String[] names = new String[n_fragments];
 				Cipher[] ciphers = new Cipher[n_fragments];
-				
+
 				for(int j = 0; j < n_fragments; j++) {
 					SecretKey ks = Cryptography.generateSecretKey(this.secret_key_gen_algorithm, this.secret_key_gen_provider, this.secret_key_size);
 					byte[] iv = Cryptography.createIV(sr, this.iv_size);
-					
+
 					ciphers[j] =  getCipher(ks, iv, Cipher.ENCRYPT_MODE);
-					
+
 					names[j] = this.convertPath(path + ".fragment-" + j);
-					
+
 					fragments_meta[j] = new client.proxy.inodes.FileDescriptor.FragmentMetaData(names[j], ks, iv);
 				}
 				DataFragment[] fragments = DataFragment.fragment(data, fragment_size, names, ciphers, sig, myKeyPair.getPrivate(), sr);
-				
+
 				((DirectoryInode)i).addChild(new client.proxy.inodes.FileDescriptor(f.getName(), fragments_meta));
-				
+
 				return fragments;
 			}
 		}
@@ -166,24 +174,24 @@ public class EncryptedFileSystem {
 
 		throw new FileNotFoundException();
 	}
-	
+
 	public byte[] assemble(client.proxy.inodes.FileDescriptor fd, byte[][] raw_fragments) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchProviderException, ShortBufferException, IllegalBlockSizeException, BadPaddingException, IOException {
-		
+
 		FragmentMetaData[] meta = fd.getFragmentsMetaData();
-		
+
 		DataFragment[] fragments = new DataFragment[meta.length];
-		
+
 		for(int i = 0; i < meta.length; i++) {
 			Cipher decryptCipher = getCipher(meta[i].ks, meta[i].iv, Cipher.DECRYPT_MODE);
 			fragments[i] = DataFragment.deserialize(meta[i].name, raw_fragments[i], encrypted_payload_size, decryptCipher);
 		}
-		
+
 		return DataFragment.defragment(fragments);
 	}
 
 	public Map<String, String> copy(String origin, String dest) throws FileNotFoundException {
 		FileDescriptor fd = this.getFileDescriptor(origin);
-		
+
 		java.io.File f = new java.io.File(dest);
 		DirectoryInode i = (DirectoryInode) this.root.getInode(f.getParent());
 
@@ -191,28 +199,28 @@ public class EncryptedFileSystem {
 			if(i.isDirectory()) {
 				FragmentMetaData[] origin_fragments_meta = fd.getFragmentsMetaData();
 				int n_fragments = origin_fragments_meta.length;
-				
+
 				FragmentMetaData[] fragments_meta = new FragmentMetaData[n_fragments];
-				
+
 				Map<String, String> copies = new HashMap<>(n_fragments);
-				
+
 				for(int j = 0; j < n_fragments; j++) {
 					SecretKey ks = origin_fragments_meta[j].ks;
 					byte[] iv = origin_fragments_meta[j].iv;
-					
+
 					String name = this.convertPath(dest + ".fragment-" + j);
-					
+
 					fragments_meta[j] = new client.proxy.inodes.FileDescriptor.FragmentMetaData(name, ks, iv);
-					
+
 					copies.put(origin_fragments_meta[j].name, name);
 				}
 
 				i.addChild(new client.proxy.inodes.FileDescriptor(f.getName(), fragments_meta));
-				
+
 				return copies;
 			}
 		}
-		
+
 		return null; // TODO: O que fazer?
 	}
 
@@ -243,15 +251,63 @@ public class EncryptedFileSystem {
 	}
 
 	public String getFileMetadata(String path) throws FileNotFoundException {
-		
+
 		int idx = path.lastIndexOf(".");
 		String ext =  idx > 0 ? path.substring(idx) : "-";
-		
+
 		Inode i = this.root.getInode(path);
-		
+
 		String metadata = String.format("%s\t%s\t%s\tCreated on: %s\tLast Access on: %s\n", path, i.isDirectory() ? "D" : "F", ext , new Date(i.getCreatedTime()), new Date(i.getLastAccess()));
-		
+
 		return metadata;
+	}
+
+	public static EncryptedFileSystem fromConfig(String path) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException, IOException, KeyStoreException, CertificateException {
+		Properties properties = IO.loadProperties(path);
+
+		int fragment_size = Integer.parseInt(properties.getProperty("FRAGMENT-SIZE", "1024"));
+		String secret_key_gen_algorithm = properties.getProperty("SECRET-KEY-GEN-ALG", "AES");
+		String secret_key_gen_provider = properties.getProperty("SECRET-KEY-GEN-PROVIDER", "BC");
+		int secret_key_size = Integer.parseInt(properties.getProperty("SECRET-KEY-SIZE", "256"));
+		int iv_size = Integer.parseInt(properties.getProperty("IV-SIZE", "16"));
+		String cipher_algorithm = properties.getProperty("CIPHER-ALGORITHM", "AES/CBC/PKCS5Padding");
+		String cipher_provider = properties.getProperty("CIPHER-PROVIDER", "BC");
+
+		String signature_alg = properties.getProperty("SIGNATURE-ALGORITHM", "SHA512withRSA");
+		String signature_provider = properties.getProperty("SIGNATURE-PROVIDER", "BC");
+		Signature sig = null;
+		if(signature_provider != null)
+			sig = Signature.getInstance(signature_alg, signature_provider);
+		else
+			sig = Signature.getInstance(signature_alg);
+
+		String hash_alg = properties.getProperty("HASH-ALGORITHM", "SHA-512");
+		String hash_provider = properties.getProperty("HASH-PROVIDER", "BC");
+		MessageDigest hash_function = null;
+		if(hash_provider != null)
+			hash_function = MessageDigest.getInstance(hash_alg, hash_provider);
+		else
+			hash_function = MessageDigest.getInstance(hash_alg);
+		
+		String sr_alg = properties.getProperty("SECURE-RANDOM-ALGORITHM", "sha1PRNG");
+		String sr_provider = properties.getProperty("SECURE-RANDOM-ALGORITHM", "JCE");
+		SecureRandom sr = null;
+		if(sr_provider != null)
+			sr = SecureRandom.getInstance(sr_alg, sr_provider);
+		else
+			sr = SecureRandom.getInstance(sr_alg);
+		
+		String keystore_location = properties.getProperty("KEYSTORE-PATH");
+		String keystore_type = properties.getProperty("KEYSTORE-TYPE");
+		String keystore_password = properties.getProperty("KEYSTORE-PASSWORD");
+		String certificate_alias  = properties.getProperty("CERTIFICATE-ALIAS");
+
+		MyKeyStore ks = new MyKeyStore(keystore_location, keystore_password, keystore_type);
+		KeyStore.PrivateKeyEntry e = (PrivateKeyEntry) ks.getEntry(certificate_alias);
+
+		KeyPair myKeyPair = new KeyPair(e.getCertificate().getPublicKey(), e.getPrivateKey());
+
+		return new EncryptedFileSystem(fragment_size, secret_key_gen_algorithm, secret_key_gen_provider, secret_key_size, iv_size, cipher_algorithm, cipher_provider, sig, myKeyPair, hash_function, sr);
 	}
 
 }
